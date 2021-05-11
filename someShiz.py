b6d720e841a932a27b470b80d86618717af14064
Zogzton Pride <kasper.skjaerbaek7@gmail.com>
2021-05-11 12:57:50 +0200

    Further work on pyton testing

--- someShiz.py
+++ someShiz.py
@@ -1,10 +1,13 @@
 aNumber = 50
 print('Hello World')
 print("Eyo " * aNumber)
-
 x = 2145.235
 print(x)
-x = input("Enter some string: ")
-print(x)
-x += " Eyo Dere"
-print(x)
+aList = [1,2,3,4,5]
+print(aList * 5)
+print(aList + [[1,2,3],[7,8,9]])
+print(list(range(5,69,5)))
+nums = [1,2,3,4,5,7]
+nums.append(1)
+print(len(nums))
+print(nums)
